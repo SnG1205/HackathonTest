e9f172781d14cd486f5559220bfc81560612dfc3
adamibra94 <91664787+adamibra94@users.noreply.github.com>
2024-05-09 11:52:42 +0200

    clean up

--- src/main/resources/static/js/navigation.js
+++ src/main/resources/static/js/navigation.js
@@ -1,40 +1,4 @@
-// Funktionen zur Navigation
-function showHome() {
-    const homeSection = document.getElementById('home');
-    const aboutSection = document.getElementById('about');
-    homeSection.style.opacity = 0;
-    aboutSection.style.opacity = 0;
-    setTimeout(() => {
-        homeSection.style.opacity = 1;
-        aboutSection.style.display = 'none';
-        homeSection.style.display = 'block';
-    }, 10);
-}
 
-function showAbout() {
-    const homeSection = document.getElementById('home');
-    const aboutSection = document.getElementById('about');
-    const searchForm = document.getElementById('lawyerSearchForm');
-    const resultsContainer = document.getElementById('results');
-
-    // Elemente ausblenden
-    homeSection.style.opacity = 0;
-    searchForm.style.display = 'none';
-    resultsContainer.style.display = 'none';
-
-    // Überprüfe, ob die Suchergebnisse vorhanden sind, und setze das Ergebnisfeld zurück
-    if (resultsContainer.firstChild) {
-        resultsContainer.innerHTML = '';
-    }
-
-    // Text für "About"-Abschnitt anzeigen
-    aboutSection.style.opacity = 0;
-    setTimeout(() => {
-        aboutSection.style.opacity = 1;
-        homeSection.style.display = 'none';
-        aboutSection.style.display = 'block';
-    }, 10);
-}
 
 // Zentrale Funktion zur Anzeige der Abschnitte
 function showSection(sectionId) {
